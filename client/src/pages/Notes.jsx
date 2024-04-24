a1ac94f8d2ef591afcf89c9708f105dd2246ef6c
thodorisKall <105176440+thodorisKall@users.noreply.github.com>
2024-04-25 01:19:53 +0300

    docs: bulid the basic component of the page to display the details of every note

---
+++ client/src/pages/Notes.jsx
@@ -0,0 +1,43 @@
+import React from "react"
+import axios from "axios"
+import { useState, useEffect } from "react"
+
+const Notes = () => {
+  const [notes, setNotes] = useState([])
+  const fetchAllNotes = async () => {
+    try {
+      const allNotes = await axios.get("http://localhost:8080/notes")
+      setNotes(allNotes.data)
+    } catch (err) {
+      if (err) console.log(`Error fetching notes from server: ${err.message}`)
+    }
+  }
+
+  useEffect(() => {
+    fetchAllNotes()
+  }, [])
+
+  return (
+    <main>
+      <h1>Notes</h1>
+      <section>
+        <div>
+          {notes.map((note) => {
+            return (
+              <div className='note_box' key={note.id}>
+                <h2>{note.title}</h2>
+                <p>{note.text}</p>
+                <h4>{note.date_edited}</h4>
+                <h4>{note.time_edited}</h4>
+                <p>---------</p>
+                <br />
+              </div>
+            )
+          })}
+        </div>
+      </section>
+    </main>
+  )
+}
+
+export default Notes
