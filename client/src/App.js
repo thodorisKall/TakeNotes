5c0f2916348d0d7d6cd2ad720b9b3d13497b476a
thodorisKall <105176440+thodorisKall@users.noreply.github.com>
2024-04-25 01:18:10 +0300

    docs: use react router and create routes for Components and not Nav

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,21 @@
 import "./App.css"
-
+import Notes from "./pages/Notes"
+import Nav from "./pages/Nav"
+import Add from "./pages/Add"
+import { BrowserRouter, Routes, Route } from "react-router-dom"
 function App() {
-  return <div className='App'>App</div>
+  return (
+    <div className='App'>
+      <BrowserRouter>
+        <Nav />
+        <Routes>
+          <Route path='/notes' element={<Notes />} />
+          <Route path='/add' element={<Add />} />
+          {/* <Route path="/users" element={<Users />} / */}
+        </Routes>
+      </BrowserRouter>
+    </div>
+  )
 }
 
 export default App
