2d01bf87e615785c4727cba251e46b7bb29f8029
thodorisKall <105176440+thodorisKall@users.noreply.github.com>
2024-04-28 00:11:46 +0300

    feat: add Home icon in home button

--- client/src/pages/Nav.jsx
+++ client/src/pages/Nav.jsx
@@ -1,15 +1,19 @@
 import { React } from "react"
 import { Link } from "react-router-dom"
+import { IoHomeOutline } from "react-icons/io5"
 
 const Nav = () => {
   return (
     <nav className='nav_container'>
       <h1>Take Note</h1>
-      <div>
-        <button>
-          <Link to='notes'>Home</Link>
+      <div className='nav_buttons'>
+        <button id='home_btn'>
+          <Link to='notes'>
+            <IoHomeOutline />
+            Home
+          </Link>
         </button>
-        <button>
+        <button id='create_btn'>
           <Link to='/add'>+Create</Link>
         </button>
       </div>
